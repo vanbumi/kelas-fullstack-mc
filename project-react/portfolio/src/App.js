ef1a687bce9cbea4061eb069e3a937826df0cb29
vanbumi <jsp.dio@gmail.com>
2019-03-06 16:26:56 +0700

    Materi SetState done

--- project-react/portfolio/src/App.js
+++ project-react/portfolio/src/App.js
@@ -4,6 +4,14 @@ class App extends Component {
   constructor() {
     super();
     this.state = { displayBio: false };
+
+    console.log('Component this', this);
+
+    this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+  }
+
+  toggleDisplayBio() {
+    this.setState({ displayBio: !this.state.displayBio });
   }
 
   render() {
@@ -18,8 +26,13 @@ class App extends Component {
               <p>I live in Bekasi, and doing code everyday</p>
               <p>Ngoding is my hobby, I love JavaScript language, and my favorite is React, React is awasome</p>
               <p>If I am not ngoding I will sleep or watch the TV.</p>
+              <button onClick={this.toggleDisplayBio}>Show less</button>
             </div>
-          ) : null
+          ) : (
+              <div>
+                <button onClick={this.toggleDisplayBio}>Read more</button>
+              </div>
+            )
         }
       </div>
     )

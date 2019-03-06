401582fe4220143558e9719fd747f700d4fa6f5d
vanbumi <jsp.dio@gmail.com>
2019-03-06 11:35:17 +0700

    12 Materi State done

--- project-react/portfolio/src/App.js
+++ project-react/portfolio/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: false };
+  }
+
   render() {
     return (
       <div>
-        <h1>John Due</h1>
+        <h1>John Doe</h1>
         <p>I am Software Engineer</p>
         <p>I am Looking forward to work in your meaningful projects.</p>
+        {
+          this.state.displayBio ? (
+            <div>
+              <p>I live in Bekasi, and doing code everyday</p>
+              <p>Ngoding is my hobby, I love JavaScript language, and my favorite is React, React is awasome</p>
+              <p>If I am not ngoding I will sleep or watch the TV.</p>
+            </div>
+          ) : null
+        }
       </div>
     )
   }

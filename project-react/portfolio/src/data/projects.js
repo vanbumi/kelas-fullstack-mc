3caedb8eeabfd2103b72b7209aac3870906c2fbe
vanbumi <jsp.dio@gmail.com>
2019-03-07 21:57:18 +0700

    Materi Props and Project Component done

--- project-react/portfolio/src/data/projects.js
+++ project-react/portfolio/src/data/projects.js
@@ -7,21 +7,21 @@ const PROJECTS = [
     id: 1,
     title: 'Example React Application',
     description: 'A React App that I built, involving JS and core web dev concepts!',
-    link: 'https://github.com/15Dkatz/example',
+    link: 'https://github.com/vanbumi/example',
     image: project1
   },
   {
     id: 2,
     title: 'My API',
     description: 'A REST API that I built from scratch with GET and POST requests!',
-    link: 'https://github.com/15Dkatz/example',
+    link: 'https://github.com/vanbumi/example',
     image: project2
   },
   {
     id: 3,
     title: 'Operating Systems Final Project',
     description: 'My unique final project for my university Operating Systems course.',
-    link: 'https://github.com/15Dkatz/example',
+    link: 'https://github.com/vanbumi/example',
     image: project3
   }
 ];
